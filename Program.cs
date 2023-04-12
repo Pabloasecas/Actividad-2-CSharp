6aa89ac08969cc536fcbc12215f3ddbbdc05c513
Pablo <pablogcg05@gmail.com>
2023-04-12 13:29:51 +0200

    Adición de var nombre y otro Console.WriteLine

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola, ¿cuál es su nombre?");
+var nombre = Console.ReadLine();
+Console.WriteLine("Hola " + nombre);
